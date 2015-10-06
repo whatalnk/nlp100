c98e85aa2e50bbedfcd3b932412f34438168cb9a
Wataru NAKAI <watnk0806@gmail.com>
2015-10-06 13:59:05 +0900

    Chapter 5: 43. 名詞を含む文節が動詞を含む文節に係るものを抽出

---
+++ Chap05_043.py
@@ -0,0 +1,19 @@
+import os.path
+from Chap05_040 import Morph
+from Chap05_041 import Chunk, cabocha_to_chunk_list
+
+with open(os.path.normcase("output/Chapter5/neko.txt.cabocha"), "r", encoding='utf-8') as f:
+    cabocha = [l.rstrip("\n") for l in f.readlines()]
+
+sentences = cabocha_to_chunk_list(cabocha)
+with open(os.path.normcase("output/Chapter5/noun_to_verb.tsv"), "w", encoding='utf-8') as f:
+    for chunk_list in sentences:
+        for chunk in chunk_list:
+            dst = int(chunk.dst.rstrip("D"))
+            if dst != -1:
+                pos_from = [m.pos for m in chunk.morphs]
+                pos_to = [m.pos for m in chunk_list[dst].morphs]
+                if "名詞" in pos_from and "動詞" in pos_to:
+                    str_from = "".join([m.surface for m in chunk.morphs if m.pos != "記号"])
+                    str_to = "".join([m.surface for m in chunk_list[dst].morphs if m.pos != "記号"])
+                    f.write("\t".join([str_from, str_to])+"\n")
